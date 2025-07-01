1fdca27a32a5505a0cdad8c03e9b94e39bc8ee2b
M. Daeva <embedmad@gmail.com>
2025-07-01 20:25:33 +0300

    completed spl_transfer

--- week-1/solana-starter/ts/cluster1/spl_transfer.ts
+++ week-1/solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,28 +1,61 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../turbin3-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
-
-// We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+import { getWallet } from "./wallet";
+import { Commitment, Connection, PublicKey } from "@solana/web3.js";
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
+const token_decimals = 1_000_000n;
+
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("fPcP9vGoowPikgu7oTRCJKHUvSNn9N5WZhYshR4UXyo");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("7y7UhiE6wZ9FsjMgsGRqLygYQja37U5BTbPtPThyHf8s");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    const keypair = await getWallet();
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const ataFrom = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+      false,
+      commitment
+    );
+    console.log(`Your ataFrom is: ${ataFrom.address.toBase58()}`); // CeKxSzyeBojUuLVX5WnjhDHewDEzyX2oAGCSy2outzR1
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const ataTo = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to,
+      false,
+      commitment
+    );
+    console.log(`Your ataTo is: ${ataTo.address.toBase58()}`); // 5LCx6Wwgj7mygs2DyTCWw7JGvdRzMpeTbfrgKr4eRFpy
+
+    // Transfer the new token to the "toTokenAccount" we just created
+    const sig = await transfer(
+      connection,
+      keypair,
+      ataFrom.address,
+      ataTo.address,
+      keypair.publicKey,
+      token_decimals * 2n,
+      undefined,
+      { commitment }
+    );
+    console.log({ sig }); // 2nTe7FgBnWeybDVgTTyTDeCbh73mgZMs8rXDC18fH6LS6qKa5AkCJpgNqReCq9Nk7gHXq3g1L9EEh6w7fHLPAWC
+
+    const balanceTo = await connection.getTokenAccountBalance(ataTo.address);
+    console.log({ balanceTo: balanceTo.value.uiAmount }); // 2
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
