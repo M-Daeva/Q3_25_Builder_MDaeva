29b10ebbe1d83fc92f213365ac9f9edb9f911077
M. Daeva <embedmad@gmail.com>
2025-07-02 19:28:28 +0300

    completed nft_metadata

--- week-1/solana-starter/ts/cluster1/nft_metadata.ts
+++ week-1/solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,45 +1,47 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import { getWallet } from "./wallet";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
-
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
-const signer = createSignerFromKeypair(umi, keypair);
-
-umi.use(irysUploader());
-umi.use(signerIdentity(signer));
+const umi = createUmi("https://api.devnet.solana.com");
 
 (async () => {
-    try {
-        // Follow this JSON structure
-        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+  try {
+    const { secretKey } = await getWallet();
+    const keypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
+    const signer = createSignerFromKeypair(umi, keypair);
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    umi.use(irysUploader());
+    umi.use(signerIdentity(signer));
+
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+    const image =
+      "https://gateway.irys.xyz/DB6vMrkFNqapPT488Hqvi2cQFJAnMc1QYr11Ttw5fpfA";
+    const metadata = {
+      name: "Not A Bootcamp",
+      symbol: "NBC",
+      description:
+        "It's not a bootcamp. It's an elite Solana builders program (IYKYK)",
+      image,
+      attributes: [{ trait_type: "cute kitty", value: "100" }],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: image,
+          },
+        ],
+      },
+      creators: [],
+    };
+    const myUri = await umi.uploader.uploadJson(metadata);
+    console.log("Your metadata URI: ", myUri); // https://gateway.irys.xyz/2BA3CVNRnG48uEG6UVsxq6vzLf8FQ7q6NRWQEKW5wGNf
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
