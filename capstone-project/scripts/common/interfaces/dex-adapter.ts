8fca0f2b24b8c70bfe0cc630267a54c4a77be0d9
M. Daeva <embedmad@gmail.com>
2025-08-10 17:38:40 +0300

    updated codegen

---
+++ capstone-project/scripts/common/interfaces/dex-adapter.ts
@@ -0,0 +1,32 @@
+import { PublicKey } from "@solana/web3.js";
+import { N } from ".";
+
+export interface InitArgs {
+  dex: PublicKey;
+  registry?: PublicKey;
+  rotationTimeout?: N<32>;
+}
+
+export interface UpdateConfigArgs {
+  admin?: PublicKey;
+  dex?: PublicKey;
+  registry?: PublicKey;
+  isPaused?: boolean;
+  rotationTimeout?: N<32>;
+}
+
+export interface SaveRouteArgs {
+  mintFirst: PublicKey;
+  mintLast: PublicKey;
+  route: RouteItem[];
+}
+
+export interface RouteItem {
+  ammIndex: N<16>;
+  tokenOut: PublicKey;
+}
+
+export interface SwapArgs {
+  amountIn: N<64>;
+  amountOutMinimum: N<64>;
+}
